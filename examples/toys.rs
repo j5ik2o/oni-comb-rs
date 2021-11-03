fd5c36e6ca38c70ca3c76faa9db0dc2e49574fa9
Junichi Kato <j5ik2o@gmail.com>
2021-11-04 08:32:00 +0900

    wip

--- examples/toys.rs
+++ examples/toys.rs
@@ -45,6 +45,59 @@ enum Expr {
 }
 
 impl Expr {
+
+  pub fn of_global_variable_definition(name: String, value: Rc<Expr>) -> Rc<Expr> {
+    Rc::new(Expr::GlobalVariableDefinition(name, value))
+  }
+
+  pub fn of_function_definition(name: String, parameters: Vec<String>, body: Rc<Expr>) -> Rc<Self> {
+    Rc::new(Expr::FunctionDefinition(name, parameters, body))
+  }
+
+  pub fn of_function_call(name: String, args: Vec<Rc<Expr>>) -> Rc<Expr> {
+    Rc::new(Expr::FunctionCall(name, args))
+  }
+
+  pub fn of_labelled_call(name: String, args: Vec<LabelledParameter>) -> Rc<Expr> {
+    Rc::new(Expr::LabelledCall(name, args))
+  }
+
+  pub fn of_symbol(symbol: String) -> Rc<Expr> {
+    Rc::new(Expr::Symbol(symbol))
+  }
+
+  pub fn of_bool_literal(value: bool) -> Rc<Expr> {
+    Rc::new(Expr::BoolLiteral(value))
+  }
+
+  pub fn of_integer_literal(value: i64) -> Rc<Expr> {
+    Rc::new(Expr::IntegerLiteral(value))
+  }
+
+  pub fn of_array_literal(values: Vec<Rc<Expr>>) -> Rc<Expr> {
+    Rc::new(Expr::ArrayLiteral(values))
+  }
+
+  pub fn of_println(expr: Rc<Expr>) -> Rc<Expr> {
+    Rc::new(Expr::Println(expr))
+  }
+
+  pub fn of_block(block: Vec<Rc<Expr>>) -> Rc<Expr> {
+    Rc::new(Expr::Block(block))
+  }
+
+  pub fn of_while(condition: Rc<Expr>, body: Rc<Expr>) -> Rc<Expr> {
+    Rc::new(Expr::While(condition, body))
+  }
+
+  pub fn of_if(condition: Rc<Expr>, then: Rc<Expr>, else_: Option<Rc<Expr>>) -> Rc<Expr> {
+    Rc::new(Expr::If(condition, then, else_))
+  }
+
+  pub fn of_assignment(name: String, value: Rc<Expr>) -> Rc<Expr> {
+    Rc::new(Expr::Assignment(name, value))
+  }
+
   pub fn of_less_than(lhs: Rc<Expr>, rhs: Rc<Expr>) -> Rc<Expr> {
     Rc::new(Expr::Binary(Operator::LessThan, lhs, rhs))
   }
@@ -109,17 +162,17 @@ fn top_level_definition<'a>() -> Parser<'a, char, Rc<Expr>> {
 fn function_definition<'a>() -> Parser<'a, char, Rc<Expr>> {
   let define_p = space() * tag("define") - space();
   let def_args_p = ident().of_many0_sep(comma()).surround(lparen(), rparen());
-  (define_p + def_args_p + block_expr())
-    .map(|((name, args), body)| Expr::FunctionDefinition(name.to_string(), args, body))
-    .map(Rc::new)
+  let p = (define_p + def_args_p + block_expr())
+    .map(|((name, args), body)| Expr::of_function_definition(name.to_string(), args, body));
+  space() * p - space()
 }
 
 fn global_variable_definition<'a>() -> Parser<'a, char, Rc<Expr>> {
   let global_p = space() * tag("global") - space();
   let global_indent_p = global_p * ident();
   let eq = space() * tag("=") - space();
-  let p = global_indent_p - eq + expression() - semi_colon();
-  p.map(|(name, e)| Expr::GlobalVariableDefinition(name, e)).map(Rc::new)
+  let p = (global_indent_p - eq + expression() - semi_colon()).map(|(name, e)| Expr::of_global_variable_definition(name, e));
+  space() * p - space()
 }
 
 fn lines<'a>() -> Parser<'a, char, Vec<Rc<Expr>>> {
@@ -127,118 +180,121 @@ fn lines<'a>() -> Parser<'a, char, Vec<Rc<Expr>>> {
 }
 
 fn line<'a>() -> Parser<'a, char, Rc<Expr>> {
-  println().attempt()
+  let p = println().attempt()
     | lazy(while_expr).attempt()
     | lazy(if_expr).attempt()
     | lazy(for_in_expr).attempt()
     | assignment().attempt()
     | expression_line().attempt()
-    | block_expr()
+    | block_expr();
+  space() * p - space()
 }
 
 fn while_expr<'a>() -> Parser<'a, char, Rc<Expr>> {
   let while_p = space() * tag("while") - space();
   let condition = while_p * lazy(expression).surround(lparen(), rparen());
-  (condition + lazy(line))
-    .map(|(c, body)| Expr::While(c, body))
-    .map(Rc::new)
+  let p = (condition + lazy(line))
+    .map(|(c, body)| Expr::of_while(c, body));
+  space() * p - space()
 }
 
 fn for_in_expr<'a>() -> Parser<'a, char, Rc<Expr>> {
   let for_p = space() * tag("for") - space();
-  let p =
-    for_p - lparen() * ident() - tag("in") + lazy(expression) - tag("to") + lazy(expression) - rparen() + lazy(line);
-  p.map(|(((name, from), to), body)| {
-    Rc::new(Expr::Block(vec![
-      Rc::new(Expr::Assignment(name.to_string(), from)),
-      Rc::new(Expr::While(
-        Expr::of_less_than(Rc::new(Expr::Symbol(name.to_string())), to),
-        Rc::new(Expr::Block(vec![
+  let in_p = space() * tag("in") - space();
+  let to_p = space() * tag("to") - space();
+  let p = (for_p - lparen() * ident() - in_p + lazy(expression) - to_p + lazy(expression) - rparen() + lazy(line)).map(|(((name, from), to), body)| {
+    Expr::of_block(vec![
+      Expr::of_assignment(name.to_string(), from),
+      Expr::of_while(
+        Expr::of_less_than(Expr::of_symbol(name.to_string()), to),
+        Expr::of_block(vec![
           body,
-          Rc::new(Expr::Assignment(
+          Expr::of_assignment(
             name.to_string(),
             Expr::of_add(
-              Rc::new(Expr::Symbol(name.to_string())),
-              Rc::new(Expr::IntegerLiteral(1)),
+              Expr::of_symbol(name.to_string()),
+              Expr::of_integer_literal(1),
             ),
-          )),
-        ])),
-      )),
-    ]))
-  })
+          ),
+        ]),
+      ),
+    ])
+  });
+  space() * p - space()
 }
 
 fn if_expr<'a>() -> Parser<'a, char, Rc<Expr>> {
   let condition = (space() * tag("if") - space()) * lparen() * expression() - rparen();
-  (condition + line() + (tag("else") * line()).opt()).map(|((p1, p2), p3)| Rc::new(Expr::If(p1, p2, p3)))
+  let else_p = space() * tag("else") - space();
+  (condition + line() + (else_p * line()).opt()).map(|((p1, p2), p3)| Expr::of_if(p1, p2, p3))
 }
 
 fn block_expr<'a>() -> Parser<'a, char, Rc<Expr>> {
-  lazy(line)
+  let p = lazy(line)
     .of_many0()
     .surround(lbrace(), rbrace())
-    .map(|e| Expr::Block(e))
-    .map(Rc::new)
+    .map(Expr::of_block);
+  space() * p - space()
 }
 
 fn assignment<'a>() -> Parser<'a, char, Rc<Expr>> {
   let eq = space() * tag("=") - space();
-  (ident() - eq + expression() - semi_colon())
-    .map(|(name, e)| Expr::Assignment(name, e))
-    .map(Rc::new)
+  let p = (ident() - eq + expression() - semi_colon())
+    .map(|(name, expr)| Expr::of_assignment(name, expr));
+  space() * p - space()
 }
 
-
-
 fn expression_line<'a>() -> Parser<'a, char, Rc<Expr>> {
-  (expression() - semi_colon()).attempt()
+  space() * expression() - semi_colon() - space()
 }
 
 fn expression<'a>() -> Parser<'a, char, Rc<Expr>> {
-  comparative()
+  space() * comparative() - space()
 }
 
 fn println<'a>() -> Parser<'a, char, Rc<Expr>> {
   let println_p = space() * tag("println") - space();
-  (println_p * lazy(expression).surround(lparen(), rparen()) - semi_colon())
-    .map(Expr::Println)
-    .map(Rc::new)
+  let p = (println_p * lazy(expression).surround(lparen(), rparen()) - semi_colon())
+    .map(Expr::of_println);
+  space() * p - space()
 }
 
 
 fn integer<'a>() -> Parser<'a, char, Rc<Expr>> {
-  (space() * regex(Regex::new(r#"-?\d+"#).unwrap()) - space())
+  let p = regex(Regex::new(r#"-?\d+"#).unwrap())
     .convert(|s| s.parse::<i64>())
-    .map(Expr::IntegerLiteral)
-    .map(Rc::new)
+    .map(Expr::of_integer_literal);
+  space() * p - space()
 }
 
 fn multitive<'a>() -> Parser<'a, char, Rc<Expr>> {
   let aster = space() * tag("*") - space();
   let slash = space() * tag("/") - space();
 
-  chain_left1(
+  let p = chain_left1(
     primary(),
     (aster | slash).map(|e| match e {
       "*" => Expr::of_multiply,
       "/" => Expr::of_divide,
       _ => panic!("unexpected operator"),
     }),
-  )
+  );
+  space() * p - space()
 }
 
 fn additive<'a>() -> Parser<'a, char, Rc<Expr>> {
   let plus = space() * tag("+") - space();
   let minus = space() * tag("-") - space();
 
-  chain_left1(
+  let p = chain_left1(
     multitive(),
     (plus | minus).map(|e| match e {
       "+" => Expr::of_add,
       "-" => Expr::of_subtract,
       _ => panic!("unexpected operator"),
     }),
-  )
+  );
+  space() * p - space()
 }
 
 fn comparative<'a>() -> Parser<'a, char, Rc<Expr>> {
@@ -249,7 +305,7 @@ fn comparative<'a>() -> Parser<'a, char, Rc<Expr>> {
   let eqeq = space() * tag("==") - space();
   let neq = space() * tag("!=") - space();
 
-  chain_left1(
+  let p = chain_left1(
     additive(),
     (lte.attempt() | gte.attempt() | neq.attempt() | lt.attempt() | gt.attempt() | eqeq.attempt()).map(|e| match e {
       "<" => Expr::of_less_than,
@@ -260,21 +316,21 @@ fn comparative<'a>() -> Parser<'a, char, Rc<Expr>> {
       "!=" => Expr::of_not_equal,
       _ => panic!("unexpected operator"),
     }),
-  )
+  );
+  space() * p - space()
 }
 
 fn function_call<'a>() -> Parser<'a, char, Rc<Expr>> {
-  let p = lazy(expression).of_many1_sep(comma()).surround(lparen(), rparen());
-  (ident() + p)
-    .map(|(name, params)| Expr::FunctionCall(name.to_string(), params))
-    .map(Rc::new)
+  let p = (ident() +  lazy(expression).of_many1_sep(comma()).surround(lparen(), rparen()))
+    .map(|(name, params)| Expr::of_function_call(name.to_string(), params));
+  space() * p - space()
 }
 
 fn labelled_call<'a>() -> Parser<'a, char, Rc<Expr>> {
   let param = (ident() - elm_ref('=') + lazy(expression)).map(|(label, param)| LabelledParameter::new(label, param));
-  (ident() + param.of_many1_sep(comma()))
-    .map(|(name, params)| Expr::LabelledCall(name.to_string(), params))
-    .map(Rc::new)
+  let p = (ident() + param.of_many1_sep(comma()))
+    .map(|(name, params)| Expr::of_labelled_call(name.to_string(), params));
+  space() * p - space()
 }
 
 fn true_literal<'a>() -> Parser<'a, char, &'a str> {
@@ -286,15 +342,15 @@ fn false_literal<'a>() -> Parser<'a, char, &'a str> {
 }
 
 fn array_literal<'a>() -> Parser<'a, char, Rc<Expr>> {
-  lazy(expression)
+  let p = lazy(expression)
     .of_many0_sep(comma())
     .surround(lbracket(), rbracket())
-    .map(|e| Expr::ArrayLiteral(e))
-    .map(Rc::new)
+    .map(Expr::of_array_literal);
+  space() * p - space()
 }
 
 fn bool_literal<'a>() -> Parser<'a, char, Rc<Expr>> {
-  (true_literal().map(|_| Expr::BoolLiteral(true)) | false_literal().map(|_| Expr::BoolLiteral(false))).map(Rc::new)
+  (true_literal().map(|_| Expr::of_bool_literal(true)) | false_literal().map(|_| Expr::of_bool_literal(false)))
 }
 
 fn ident<'a>() -> Parser<'a, char, String> {
@@ -302,17 +358,19 @@ fn ident<'a>() -> Parser<'a, char, String> {
 }
 
 fn identifier<'a>() -> Parser<'a, char, Rc<Expr>> {
-  ident().map(Expr::Symbol).map(Rc::new)
+  let p = ident().map(Expr::of_symbol);
+  space() * p - space()
 }
 
 fn primary<'a>() -> Parser<'a, char, Rc<Expr>> {
-  (lparen() * lazy(expression) - rparen())
-    | function_call().attempt()
-    | labelled_call().attempt()
-    | array_literal().attempt()
-    | bool_literal().attempt()
-    | identifier().attempt()
-    | integer()
+  let p = (lparen() * lazy(expression) - rparen())
+      | function_call().attempt()
+      | labelled_call().attempt()
+      | array_literal().attempt()
+      | bool_literal().attempt()
+      | identifier().attempt()
+      | integer();
+  space() * p - space()
 }
 
 #[cfg(test)]
@@ -321,49 +379,147 @@ mod test {
 
   #[test]
   fn test_assignment() {
-    let source = r#"i = 1;"#;
+    let source = r#"
+    i = 1;
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = line().parse(&input).unwrap();
+    println!("{:?}", result);
     if let &Expr::Assignment(ref name, ref expr) = &*result {
       assert_eq!(name, "i");
       if let &Expr::IntegerLiteral(i) = &*(*expr) {
         assert_eq!(i, 1);
       }
+    } else {
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_println() {
-    let source = r#"println(10);"#;
+    let source = r#"
+    println(10);
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = line().parse(&input).unwrap();
+    println!("{:?}", result);
     if let &Expr::Println(ref expr) = &*result {
       if let &Expr::IntegerLiteral(i) = &*(*expr) {
         assert_eq!(i, 10);
       }
+    } else {
+      panic!("unexpected result");
+    }
+  }
+
+  #[test]
+  fn test_primary_bool_true() {
+    let source = r#"
+    true
+    "#;
+    let input = source.chars().into_iter().collect::<Vec<_>>();
+    let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
+    if let &Expr::BoolLiteral(b) = &*result {
+      assert!(b);
+    } else {
+      panic!("unexpected result");
+    }
+  }
+
+  #[test]
+  fn test_primary_bool_false() {
+    let source = r#"
+    false
+    "#;
+    let input = source.chars().into_iter().collect::<Vec<_>>();
+    let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
+    if let &Expr::BoolLiteral(b) = &*result {
+      assert!(!b);
+    }else {
+      panic!("unexpected result");
+    }
+  }
+
+  #[test]
+  fn test_primary_bool_array_0() {
+    let source = r#"
+    []
+    "#;
+    let input = source.chars().into_iter().collect::<Vec<_>>();
+    let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
+    if let Expr::ArrayLiteral(v) = &*result {
+      assert!(v.is_empty());
+    }else {
+      panic!("unexpected result");
+    }
+  }
+
+  #[test]
+  fn test_primary_bool_array_1() {
+    let source = r#"
+    [1]
+    "#;
+    let input = source.chars().into_iter().collect::<Vec<_>>();
+    let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
+    if let Expr::ArrayLiteral(v) = &*result {
+      assert!(!v.is_empty());
+      if let &Expr::IntegerLiteral(i) = &*v[0] {
+        assert_eq!(i, 1);
+      }
+    }else {
+      panic!("unexpected result");
+    }
+  }
+
+  #[test]
+  fn test_primary_bool_array_2() {
+    let source = r#"
+    [1, 2]
+    "#;
+    let input = source.chars().into_iter().collect::<Vec<_>>();
+    let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
+    if let Expr::ArrayLiteral(v) = &*result {
+      assert!(!v.is_empty());
+      if let &Expr::IntegerLiteral(i) = &*v[0] {
+        assert_eq!(i, 1);
+      }
+    }else {
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_primary_integer() {
-    let source = r#"10"#;
+    let source = r#"
+    10
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = primary().parse(&input).unwrap();
+    println!("{:?}", result);
     if let &Expr::IntegerLiteral(i) = &*result {
       assert_eq!(i, 10);
+    }else {
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_primary_identifier() {
-    let source = r#"abc"#;
+    let source = r#"
+    abc
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = primary().parse(&input).unwrap();
     println!("{:?}", result);
     if let Expr::Symbol(name) = &*result {
       assert_eq!(name, "abc");
     } else {
-      panic!()
+      panic!("unexpected result");
     }
   }
 
@@ -382,13 +538,15 @@ mod test {
         assert_eq!(*r, 2);
       }
     } else {
-      panic!()
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_additive() {
-    let source = r#"1 + 2"#;
+    let source = r#"
+    1 + 2
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = expression().parse(&input).unwrap();
     println!("{:?}", result);
@@ -401,13 +559,15 @@ mod test {
         assert_eq!(*r, 2);
       }
     } else {
-      panic!()
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_comparative() {
-    let source = r#"1>2"#;
+    let source = r#"
+    1>2
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = expression().parse(&input).unwrap();
     println!("{:?}", result);
@@ -420,13 +580,15 @@ mod test {
         assert_eq!(*r, 2);
       }
     } else {
-      panic!()
+      panic!("unexpected result");
     }
   }
 
   #[test]
   fn test_comparative_2() {
-    let source = r#"a>2"#;
+    let source = r#"
+    a>2
+    "#;
     let input = source.chars().into_iter().collect::<Vec<_>>();
     let result = expression().parse(&input).unwrap();
     println!("{:?}", result);
@@ -439,7 +601,7 @@ mod test {
         assert_eq!(*r, 2);
       }
     } else {
-      panic!()
+      panic!("unexpected result");
     }
   }
 }
