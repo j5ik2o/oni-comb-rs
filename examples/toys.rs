05c2638436695c976d484fa13bc46f589b62fbc8
Junichi Kato <j5ik2o@gmail.com>
2021-11-08 08:31:44 +0900

    wip

--- examples/toys.rs
+++ examples/toys.rs
@@ -8,39 +8,6 @@ use std::collections::HashMap;
 
 use std::rc::Rc;
 
-#[derive(Debug, Clone)]
-pub struct Environment {
-  bindings: HashMap<String, i64>,
-  next: Option<Rc<Environment>>,
-}
-
-impl Environment {
-  pub fn as_bindings_mut(&mut self) -> &HashMap<String, i64> {
-    &mut self.bindings
-  }
-
-  pub fn as_bindings(&self) -> &HashMap<String, i64> {
-    &self.bindings
-  }
-
-  pub fn find_binding(&self, name: &str) -> Option<&HashMap<String, i64>> {
-    match self.bindings.get(name) {
-      Some(_) => Some(&self.bindings),
-      None => match &self.next {
-        Some(n) => {
-          let r = (*n).find_binding(name);
-          r
-        }
-        None => None,
-      },
-    }
-  }
-
-  pub fn new(bindings: HashMap<String, i64>, next: Option<Rc<Environment>>) -> Environment {
-    Self { bindings, next }
-  }
-}
-
 #[derive(Clone, Debug, PartialEq)]
 enum Operator {
   Add,            // +
@@ -737,6 +704,39 @@ fn semi_colon<'a>() -> Parser<'a, char, &'a str> {
 
 fn space<'a>() -> Parser<'a, char, ()> {
   elm_of(" \t\r\n").of_many0().discard()
+}
+
+#[derive(Debug, Clone)]
+pub struct Environment {
+  bindings: HashMap<String, i64>,
+  next: Option<Rc<Environment>>,
+}
+
+impl Environment {
+  pub fn as_bindings_mut(&mut self) -> &HashMap<String, i64> {
+    &mut self.bindings
+  }
+
+  pub fn as_bindings(&self) -> &HashMap<String, i64> {
+    &self.bindings
+  }
+
+  pub fn find_binding(&self, name: &str) -> Option<&HashMap<String, i64>> {
+    match self.bindings.get(name) {
+      Some(_) => Some(&self.bindings),
+      None => match &self.next {
+        Some(n) => {
+          let r = (*n).find_binding(name);
+          r
+        }
+        None => None,
+      },
+    }
+  }
+
+  pub fn new(bindings: HashMap<String, i64>, next: Option<Rc<Environment>>) -> Environment {
+    Self { bindings, next }
+  }
 }
 
 struct Interpreter {
