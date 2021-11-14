fded1fa05072e56bd4e672dfb3eb2b00595ff721
Junichi Kato <j5ik2o@gmail.com>
2021-11-14 09:29:11 +0900

    wip

--- parser/src/core/parse_error.rs
+++ parser/src/core/parse_error.rs
@@ -2,7 +2,7 @@ use std::fmt;
 use std::fmt::Display;
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
-pub enum ParseError<'a, I> {
+pub enum ParsedError<'a, I> {
   Mismatch {
     input: &'a [I],
     offset: usize,
@@ -18,41 +18,41 @@ pub enum ParseError<'a, I> {
   Incomplete,
   Expect {
     offset: usize,
-    inner: Box<ParseError<'a, I>>,
+    inner: Box<ParsedError<'a, I>>,
     message: String,
   },
   Custom {
     offset: usize,
-    inner: Option<Box<ParseError<'a, I>>>,
+    inner: Option<Box<ParsedError<'a, I>>>,
     message: String,
   },
 }
 
-impl<'a, I> Display for ParseError<'a, I> {
+impl<'a, I> Display for ParsedError<'a, I> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match self {
-      ParseError::Incomplete => write!(f, "Incomplete"),
-      ParseError::Mismatch {
+      ParsedError::Incomplete => write!(f, "Incomplete"),
+      ParsedError::Mismatch {
         ref message,
         ref offset,
         ..
       } => write!(f, "Mismatch at {}: {}", offset, message),
-      ParseError::Conversion {
+      ParsedError::Conversion {
         ref message,
         ref offset,
         ..
       } => write!(f, "Conversion failed at {}: {}", offset, message),
-      ParseError::Expect {
+      ParsedError::Expect {
         ref message,
         ref offset,
         ref inner,
       } => write!(f, "{} at {}: {}", message, offset, inner),
-      ParseError::Custom {
+      ParsedError::Custom {
         ref message,
         ref offset,
         inner: Some(ref inner),
       } => write!(f, "{} at {}, (inner: {})", message, offset, inner),
-      ParseError::Custom {
+      ParsedError::Custom {
         ref message,
         ref offset,
         inner: None,
@@ -61,52 +61,52 @@ impl<'a, I> Display for ParseError<'a, I> {
   }
 }
 
-impl<'a, I> ParseError<'a, I> {
+impl<'a, I> ParsedError<'a, I> {
   pub fn is_expect(&self) -> bool {
     match self {
-      ParseError::Expect { .. } => true,
+      ParsedError::Expect { .. } => true,
       _ => false,
     }
   }
 
   pub fn is_custom(&self) -> bool {
     match self {
-      ParseError::Custom { .. } => true,
+      ParsedError::Custom { .. } => true,
       _ => false,
     }
   }
 
   pub fn is_mismatch(&self) -> bool {
     match self {
-      ParseError::Mismatch { .. } => true,
+      ParsedError::Mismatch { .. } => true,
       _ => false,
     }
   }
 
   pub fn is_conversion(&self) -> bool {
     match self {
-      ParseError::Conversion { .. } => true,
+      ParsedError::Conversion { .. } => true,
       _ => false,
     }
   }
 
   pub fn is_in_complete(&self) -> bool {
     match self {
-      ParseError::Incomplete => true,
+      ParsedError::Incomplete => true,
       _ => false,
     }
   }
 
-  pub fn of_expect(offset: usize, inner: Box<ParseError<'a, I>>, message: String) -> Self {
-    ParseError::Expect { offset, inner, message }
+  pub fn of_expect(offset: usize, inner: Box<ParsedError<'a, I>>, message: String) -> Self {
+    ParsedError::Expect { offset, inner, message }
   }
 
-  pub fn of_custom(offset: usize, inner: Option<Box<ParseError<'a, I>>>, message: String) -> Self {
-    ParseError::Custom { offset, inner, message }
+  pub fn of_custom(offset: usize, inner: Option<Box<ParsedError<'a, I>>>, message: String) -> Self {
+    ParsedError::Custom { offset, inner, message }
   }
 
   pub fn of_mismatch(input: &'a [I], offset: usize, length: usize, message: String) -> Self {
-    ParseError::Mismatch {
+    ParsedError::Mismatch {
       input,
       offset,
       length,
@@ -115,7 +115,7 @@ impl<'a, I> ParseError<'a, I> {
   }
 
   pub fn of_conversion(input: &'a [I], offset: usize, length: usize, message: String) -> Self {
-    ParseError::Conversion {
+    ParsedError::Conversion {
       input,
       offset,
       length,
@@ -124,6 +124,6 @@ impl<'a, I> ParseError<'a, I> {
   }
 
   pub fn of_in_complete() -> Self {
-    ParseError::Incomplete
+    ParsedError::Incomplete
   }
 }
--- parser/src/core/parse_result.rs
+++ parser/src/core/parse_result.rs
@@ -1,97 +1,131 @@
-use crate::core::parse_error::ParseError;
+use crate::core::committed_status::CommittedStatus;
+use crate::core::parse_error::ParsedError;
 
+/// A Parsed Result.<br/>
+/// 解析された結果。
 #[derive(Debug, Clone)]
-pub enum ParseResult<'a, I, A> {
-  Success { get: A, length: usize },
-  Failure { get: ParseError<'a, I>, is_committed: bool },
+pub enum ParsedResult<'a, I, A> {
+  /// 成功
+  Success {
+    /// 成功の値
+    value: A,
+    /// valueのサイズ
+    length: usize,
+  },
+  /// 失敗
+  Failure {
+    /// 失敗の原因
+    error: ParsedError<'a, I>,
+    /// コミット状態
+    is_committed: CommittedStatus,
+  },
 }
 
-impl<'a, I, A> ParseResult<'a, I, A> {
-  pub fn successful(get: A, length: usize) -> Self {
-    ParseResult::Success { get, length }
+impl<'a, I, A> ParsedResult<'a, I, A> {
+  /// Returns the parsed result of success.<br/>
+  /// 成功の解析結果を返します。
+  ///
+  /// - value: a value
+  /// - length: a size of the value
+  ///
+  /// - value: 値
+  /// - length: 値のサイズ
+  pub fn successful(value: A, length: usize) -> Self {
+    ParsedResult::Success { value, length }
   }
 
-  pub fn failed(get: ParseError<'a, I>, is_committed: bool) -> Self {
-    ParseResult::Failure { get, is_committed }
+  /// Returns the parsed result of failure.<br/>
+  /// 失敗の解析結果を返します。
+  ///
+  /// - error: a [ParsedError]
+  /// - is_committed: a [CommittedStatus]
+  ///
+  pub fn failed(error: ParsedError<'a, I>, is_committed: CommittedStatus) -> Self {
+    ParsedResult::Failure { error, is_committed }
   }
 
-  pub fn failed_with_un_commit(get: ParseError<'a, I>) -> Self {
-    ParseResult::Failure {
-      get,
-      is_committed: false,
+  /// Returns the parsed result of failure.<br/>
+  /// 失敗の解析結果を返します。
+  ///
+  /// - error: a [ParsedError]
+  pub fn failed_with_un_commit(error: ParsedError<'a, I>) -> Self {
+    ParsedResult::Failure {
+      error,
+      is_committed: CommittedStatus::Uncommitted,
     }
   }
 
-  pub fn failed_with_commit(get: ParseError<'a, I>) -> Self {
-    ParseResult::Failure {
-      get,
-      is_committed: true,
+  pub fn failed_with_commit(error: ParsedError<'a, I>) -> Self {
+    ParsedResult::Failure {
+      error,
+      is_committed: CommittedStatus::Committed,
     }
   }
 
-  pub fn to_result(self) -> Result<A, ParseError<'a, I>> {
+  /// Convert [ParsedResult] to [Result].
+  ///
+  /// [ParsedResult]を[Result]に変換する。
+  pub fn to_result(self) -> Result<A, ParsedError<'a, I>> {
     match self {
-      ParseResult::Failure { get: e, .. } => Err(e),
-      ParseResult::Success { get: a, .. } => Ok(a),
+      ParsedResult::Failure { error, .. } => Err(error),
+      ParsedResult::Success { value, .. } => Ok(value),
     }
   }
 
   pub fn is_success(&self) -> bool {
     match self {
-      ParseResult::Failure { .. } => false,
-      ParseResult::Success { .. } => true,
+      ParsedResult::Failure { .. } => false,
+      ParsedResult::Success { .. } => true,
     }
   }
 
   pub fn success(self) -> Option<A> {
     match self {
-      ParseResult::Failure { .. } => None,
-      ParseResult::Success { get: a, .. } => Some(a),
+      ParsedResult::Failure { .. } => None,
+      ParsedResult::Success { value, .. } => Some(value),
     }
   }
 
-  pub fn failure(self) -> Option<ParseError<'a, I>> {
+  pub fn failure(self) -> Option<ParsedError<'a, I>> {
     match self {
-      ParseResult::Failure { get: e, .. } => Some(e),
-      ParseResult::Success { .. } => None,
+      ParsedResult::Failure { error, .. } => Some(error),
+      ParsedResult::Success { .. } => None,
     }
   }
 
   pub fn is_failure(&self) -> bool {
     match self {
-      ParseResult::Failure { .. } => true,
-      ParseResult::Success { .. } => false,
+      ParsedResult::Failure { .. } => true,
+      ParsedResult::Success { .. } => false,
     }
   }
 
-  pub fn is_committed(&self) -> Option<bool> {
+  pub fn is_committed(&self) -> Option<CommittedStatus> {
     match self {
-      &ParseResult::Failure { is_committed, .. } => Some(is_committed),
+      ParsedResult::Failure { is_committed, .. } => Some(is_committed.clone()),
       _ => None,
     }
   }
 
+  /// 失敗時のコミットを解除する
   pub fn with_un_commit(self) -> Self {
     match self {
-      ParseResult::Failure {
-        get: e,
-        is_committed: true,
-      } => ParseResult::Failure {
-        get: e,
-        is_committed: false,
+      ParsedResult::Failure {
+        error,
+        is_committed: CommittedStatus::Committed,
+      } => ParsedResult::Failure {
+        error,
+        is_committed: CommittedStatus::Uncommitted,
       },
       _ => self,
     }
   }
 
-  pub fn with_committed_fallback(self, is_committed: bool) -> Self {
+  pub fn with_committed_fallback(self, is_committed: CommittedStatus) -> Self {
     match self {
-      ParseResult::Failure {
-        get: e,
-        is_committed: c,
-      } => ParseResult::Failure {
-        get: e,
-        is_committed: c || is_committed,
+      ParsedResult::Failure { error, is_committed: c } => ParsedResult::Failure {
+        error,
+        is_committed: (c | is_committed),
       },
       _ => self,
     }
@@ -99,13 +133,13 @@ impl<'a, I, A> ParseResult<'a, I, A> {
 
   pub fn map_err<F>(self, f: F) -> Self
   where
-    F: Fn(ParseError<'a, I>) -> ParseError<'a, I>, {
+    F: Fn(ParsedError<'a, I>) -> ParsedError<'a, I>, {
     match self {
-      ParseResult::Failure {
-        get: e,
+      ParsedResult::Failure {
+        error: e,
         is_committed: c,
-      } => ParseResult::Failure {
-        get: f(e),
+      } => ParsedResult::Failure {
+        error: f(e),
         is_committed: c,
       },
       _ => self,
@@ -114,7 +148,7 @@ impl<'a, I, A> ParseResult<'a, I, A> {
 
   pub fn with_add_length(self, n: usize) -> Self {
     match self {
-      ParseResult::Success { get: a, length: m } => ParseResult::Success { get: a, length: n + m },
+      ParsedResult::Success { value, length: m } => ParsedResult::Success { value, length: n + m },
       _ => self,
     }
   }
