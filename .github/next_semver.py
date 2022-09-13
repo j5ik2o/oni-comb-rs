c50f9360b2e91b7bc626a93f8bf6331b1e600dd9
Junichi Kato <j5ik2o@gmail.com>
2022-09-13 23:21:45 +0900

    wip

--- .github/next_semver.py
+++ .github/next_semver.py
@@ -8,6 +8,6 @@ for s in sys.stdin:
     r = re.match('.*v?(\d\.\d\.\d)', s)
     print(r)
     if r:
-        cur_ver = semver.VersionInfo.parse(r.group(0))
+        cur_ver = semver.VersionInfo.parse(r.group(1))
         next_ver = cur_ver.bump_patch()
         print(next_ver)
