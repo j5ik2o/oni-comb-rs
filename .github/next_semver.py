cdba863c4671a8f72560dce30d46b3b32a4b9193
Junichi Kato <j5ik2o@gmail.com>
2022-09-13 23:17:38 +0900

    wip

--- .github/next_semver.py
+++ .github/next_semver.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 import sys
 import semver
+import re
 
 for s in sys.stdin:
-    cur_ver = semver.VersionInfo.parse(s)
-    next_ver = cur_ver.bump_patch()
-    print(next_ver)
+    r = re.match('.*v(\d\.\d\.\d)', s)
+    print(r)
+    if r:
+        cur_ver = semver.VersionInfo.parse(r.group(0))
+        next_ver = cur_ver.bump_patch()
+        print(next_ver)
