5a790804fa2ec4db407934046f45ebfca755a844
Junichi Kato <j5ik2o@gmail.com>
2022-09-14 02:04:18 +0900

    Update create-release-note.py

--- .github/create-release-note.py
+++ .github/create-release-note.py
@@ -4,9 +4,9 @@ import sys
 import csv
 import re
 
-d = {'build': [], 'ci': [], 'feat': [], 'fix': [], 'docs': [], 'style': [], 'refactor': [], 'perf': [], 'test': [], 'revert': [], 'chore': []}
+commit_messages = {'build': [], 'ci': [], 'feat': [], 'fix': [], 'docs': [], 'style': [], 'refactor': [], 'perf': [], 'test': [], 'revert': [], 'chore': []}
 
-tiles = {'build': 'Build Systems', 'ci': 'Continuous Integration', 'feat': 'Features', 'fix': 'Bug Fixes', 'docs': 'Documentation',
+titles = {'build': 'Build Systems', 'ci': 'Continuous Integration', 'feat': 'Features', 'fix': 'Bug Fixes', 'docs': 'Documentation',
          'style': 'Styles', 'refactor': 'Code Refactoring', 'perf': 'Performance Improvements', 'test': 'Tests',
          'revert': 'Reverts', 'chore': 'Chores'}
 
@@ -15,16 +15,16 @@ cin = csv.reader(sys.stdin, delimiter="\t") # 補足: 開く対象がファイ�
 def match_append(key, row):
     r = re.match(f"^{key}(.*)?\: (.*)", row[2])
     if r:
-        d[key].append((r.group(2), row))
+        commit_messages[key].append((r.group(2), row))
 
 for row in cin:
-    for key in d.keys():
+    for key in commit_messages.keys():
         match_append(key, row)
 
-for k,v in tiles.items():
-    if d[k]:
+for k,v in titles.items():
+    if commit_messages[k]:
         print(f"### {v}\n")
-    for messages in d[k]:
+    for messages in commit_messages[k]:
         print(f"* {messages[0]} ({messages[1][0]})")
-    if d[k]:
+    if commit_messages[k]:
         print()
