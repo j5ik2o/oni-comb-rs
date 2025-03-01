2a9e56183cd6af49deb2da42750173e6333bb35e
Junichi Kato <j5ik2o@gmail.com>
2025-03-01 08:38:33 +0000

    fix: update module structure for internal module
    Co-Authored-By: 加藤潤一 <junichi.kato@ideo-plus.jp>

---
+++ parser/src/internal/mod.rs
@@ -0,0 +1,7 @@
+pub mod parser_impl;
+pub mod parsers_impl;
+pub mod static_parser_impl;
+pub mod static_parsers_impl;
+
+pub use crate::internal::parser_impl::ParserImpl;
+pub use crate::internal::parsers_impl::ParsersImpl;
